ee9bb7000613263a8f71ed7684f550f4bba7130a
Анастасия Мамаева <hdekjsne@gmail.com>
2023-10-28 14:14:59 +0300

    validations for string

--- src/StringSchema.js
+++ src/StringSchema.js
@@ -1,7 +1,32 @@
 export default class StringSchema {
-	constructor() {}
+	constructor() {
+		this.isFirstCapital = undefined;
+		this.l = undefined;
+		this.exclamation = undefined;
+	}
+
+	startsFromUpperCase() {
+		this.isFirstCapital = true;
+		return this;
+	}
+
+	length(l) {
+		this.l = l;
+		return this;
+	}
+
+	hasExclamation() {
+		this.exclamation = true;
+		return this;
+	}
 
 	isValid(value) {
-		return typeof value === 'string';
+		const validations = [
+			typeof value === 'string',
+			this.isFirstCapital !== undefined ? /^[A-Z]/.test(value) : true,
+			this.l !== undefined ? value.length === this.l : true,
+			this.exclamation !== undefined ? value.includes('!') : true,
+		];
+		return !validations.includes(false);
 	}
 }
