0ce7ead2b18a83ed4be7dabd1e5c5962dceffe48
Анастасия Мамаева <hdekjsne@gmail.com>
2023-10-28 18:56:41 +0300

    final step

--- src/ObjectSchema.js
+++ src/ObjectSchema.js
@@ -1,5 +1,21 @@
 export default class ObjectSchema {
-	constructor() {}
+	constructor() {
+		this.example = undefined;
+	}
 
-	isValid(value) {}
+	shape(obj) {
+		this.example = obj;
+		return this;
+	}
+
+	isValid(value) {
+		const validations = [
+			Object.keys(value).length === Object.keys(this.example).length,
+			Object.entries(value).reduce((acc, [key, value]) => {
+				if (acc === false) return acc;
+				return this.example[key].isValid(value);
+			}, true),
+		];
+		return !validations.includes(false);
+	}
 }
